fd3c5d6650101ed910a6697cb6957ad651983568
Krzysztof95 <krzysztof.pietzrak.95@poczta.fm>
2019-10-03 14:48:30 +0200

    WSB

---
+++ Projekt ONE.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Komentarz
+            *komentarz
+            * w
+            * wielu
+            * liniach
+             *
+            Console.WriteLine("Janusz");
+            Console.ReadKey();
+            int x = 10;
+            z = 15;
+            Console.WriteLine(x1);
+            Console.Writeline(y = " + y + "\nz = z" + z); 
+            int a = 10;
+            int b = a;
+            //alternatywny
+            //int c= 0;
+            int x = new int();
+            Console.Write(Podaj imię)
+            string name = Console.ReadLine();
+            int z1 = 12;
+
+            uint z2 = 20;
+
+           float y1 = 15;
+
+            /*
+             *Oblicz pola prostokata
+             */
+
+            string y = Console.ReadLine();
+            double x = double.Parse(y);
+            double x = double.Parse(Console.ReadLine()); 
+
